1a2c8cb41c4a512b3656e5cdde8180aa3fe14595
Wallison Ferreira <sanfer_wallison@hotmail.com>
2022-11-08 20:44:48 -0500

    Aula de Terça

--- first-week/js/index.js
+++ first-week/js/index.js
@@ -188,5 +188,87 @@ function fora() {
     return somarXMais5()
 }
 
-() => ( (x = 7) => (x + 5) )
+const sumValues = () => ( (x = 7) => (x + 5) ) ()
 
+console.log(sumValues())
+
+const testArrow = () => ((x=7), ()=>(x+5))  ()
+console.log(testArrow())
+
+console.log('#################### Aula Terça ####################')
+
+const namesLength = names.map( ({ length }) => length )
+
+console.log(namesLength)
+
+// Desafio A+: Fazer esquema em que um código declara uma variável (let) e uma função faz a soma do valor da variável com um valor estático (5, por exemplo) como retorno. Outro código importa o primeiro e declara uma variável com o mesmo nome e outro valor. Em seguida, chama a função do primeiro código. Qual valor será exibido?
+// Utilizar o artigo disponível no endereço https://www.horadecodar.com.br/2020/08/13/como-incluir-um-arquivo-de-javascript-em-outro/
+
+// Orientação a objetos
+
+class Product{
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+
+    productDetails() {
+        return `O nome do produto é ${this.name} e o preço é ${this.price}`
+    }
+
+    static test() {
+        console.log('testando método estático...')
+    }
+}
+
+// instanciando um objeto product
+
+const shirt = new Product('Camisa branca', 19.99)
+console.log(shirt.productDetails())
+
+const sock = new Product('Meia cinza', 12.49)
+console.log(sock.productDetails())
+
+Product.test()
+
+// Herança
+
+class Tenis extends Product {
+    constructor(name, price, size) {
+        super(name, price)
+        this.size = size
+    }
+
+    showNumber() {
+        return `O tamanho do ${this.name} é ${this.size}`
+    }
+
+    productDetails() {
+        return `O nome do produto é ${this.name}, \no tamanho é ${this.size} e o preço é ${this.price}`
+    }
+}
+
+const tenis = new Tenis('Tenis Nike Jordan', 12000.00, 42)
+
+console.log(tenis.showNumber(), tenis.productDetails())
+
+// Document object Model (DOM)
+
+//console.log(document.getElementsByClassName('text'))
+
+const texts = document.querySelectorAll('.text')
+console.log(texts)
+
+texts.forEach((data) => console.log(data))
+
+texts[0].textContent = 'Estou alterando o primeiro parágrafo'
+
+texts[0].innerHTML = '<span> Testando uma alteração</span>'
+
+texts[1].style.backgroundColor = '#c6c6c4'
+
+texts[2].remove()
+
+const button = document.querySelector('#btn')
+
+button.addEventListener('click', () => (texts[3].style.backgroundColor = 'orange'))
